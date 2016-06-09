52bd846f666e5e64740a939491db1adde3ceaed7
Darren <darren@darrenmothersele.com>
2016-06-09 13:52:25 +0100

    add gui example

---
+++ examples/06-editor.cpp
@@ -0,0 +1,32 @@
+#include <cyfw/main.h>
+
+
+using namespace std;
+using namespace cy;
+
+class MyApp : public cy::App
+{
+    Gui gui;
+public:
+    void setup()
+    {
+        gui.init(window);
+    }
+
+    void draw()
+    {
+        window->clear();
+        gui.clear();
+        gui.demo();
+        gui.draw();
+    }
+
+    void key(window::KeyEvent e) { gui.key(e); }
+    void scroll(window::ScrollEvent e) { gui.scroll(e); }
+    void textInput(window::CharEvent e) { gui.character(e); }
+    void mouseButton(window::MouseButtonEvent e ) { gui.mouse(e); }
+};
+
+int main() {
+    cy::run<MyApp,ConsoleLogger>(640, 480, "testing text input");
+}
